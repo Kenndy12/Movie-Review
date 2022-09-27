5c041ddef3451b8347aa2b77cf357450af22a268
Kenndy <justkenndy92@gmail.com>
2022-09-27 10:49:39 +0800

    Improved some UI and added an SVG background

--- client/src/components/Posts/Posts.js
+++ client/src/components/Posts/Posts.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Grid, CircularProgress} from '@material-ui/core';
 import Post from "./Post/Post";
 
 import { useSelector } from 'react-redux';
@@ -10,12 +11,16 @@ const Posts = () => {
     const classes = useStyles();
 
     console.log(posts);
-    return (
-        <>
-            <h1 className={classes.mainContainer}>POSTS</h1>
-            <Post />
-            <Post />
-        </>
+    return (        
+        !posts.length ? <CircularProgress/> : (
+            <Grid className={classes.container} container alignItems="stretch" spacing ={3}>
+                {posts.map((post) => (
+                    <Grid key={post._id} item xs={12} sm={6}>
+                        <Post post={post} />
+                    </Grid>
+                ))}
+            </Grid>
+        )
     );
 }
 
