ff4fd63e62ad4a97447aa5bcfa4123120d3a736f
Kenndy <justkenndy92@gmail.com>
2022-10-10 10:21:04 +0800

    Fixed layout of webpages and added some packages

---
+++ client/src/components/Auth/Auth.js
@@ -0,0 +1,14 @@
+import { Typography } from "@material-ui/core";
+import React from "react";
+
+const Auth = () => {
+    const state = null;
+
+    return (
+        <div>
+            <Typography> TEST </Typography>
+        </div>
+    )
+}
+
+export default Auth
